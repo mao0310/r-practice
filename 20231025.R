15a20edfea145e5315ed1f8b1274350b68017bf7
mao0310 <144878746+mao0310@users.noreply.github.com>
2023-11-01 15:04:59 +0800

    Add files via upload

---
+++ 20231025.R
@@ -0,0 +1,33 @@
+install.packages("dplyr")
+flights = readRDS("data/flights.rds")
+flightsData <- flights$data[[1]]$data_frame
+dplyr::glimpse(flightsData)
+
+
+levels(flightsData$AirlineID) |> length()
+
+
+levels(flightsData$AirlineID) |> 
+  length()
+
+length(levels(flightsData$Airline))
+
+sum(c(29, 23, 1, 17))
+
+c(29, 23, 1, 17) |> sum()
+
+c(1, 2, 2, 4, 6, 4) |> 
+  table()
+
+flightsData$AirlineID |>
+  table() -> tb_airlineID
+
+levels(flightsData$AirlineID)
+
+class(tb_airlineID)
+typeof(tb_airlineID)
+
+tb_airlineID |> sort(decreasing = TRUE)
+sort(tb_airlineID, decreasing = TRUE)
+
+sort(x)
