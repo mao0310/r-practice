9c56a0b280d27f71d877b19a8a936a63770e719d
mao0310 <144878746+mao0310@users.noreply.github.com>
2023-12-06 14:24:18 +0800

    Add files via upload

---
+++ 20231206.R
@@ -0,0 +1,31 @@
+# a block of program
+# code block
+
+# example of a code block
+# programming block
+{
+  # code lines
+}
+
+# use it repetitively
+# add for(...) in front of it
+for(.x in ...)
+{
+  # code lines
+}
+
+#use the block somewhere else
+# add function(...) in front of it
+# and bind it with a name
+myfun <- function(...)
+{
+  
+}
+
+# at anywhere you want to use the code block
+myfun(...) # function call
+
+
+
+
+
